e3eaa032afc2e0d4d8447687167330dc279178bc
Bob Zurad <bob.zurad@mimacom.com>
2020-09-14 22:31:10 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className="container">
+    <h1>NoteFire 2</h1>
+  </div>
+);
+
+export default App;

557b85512082ae30e8d04cdf8e64d07411ce9de8
Bob Zurad <bob.zurad@mimacom.com>
2020-09-14 22:59:43 -0500

    added material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import './App.scss';
 
 const App = () => (
   <div className="container">
-    <h1>NoteFire 2</h1>
+    <Card>
+      <CardContent>
+        <h1>NoteFire 2</h1>
+      </CardContent>
+    </Card>
   </div>
 );
 
